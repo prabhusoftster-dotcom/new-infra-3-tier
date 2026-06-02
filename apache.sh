21ef7a1688c6b107789c15fe2fd0582f6d742fde
prabhusoftster-dotcom <prabhusoftster@gmail.com>
2026-06-02 18:37:37 +0530

    Update apache.sh

--- apache.sh
+++ apache.sh
@@ -1,7 +1,15 @@
-#! /bin/bash
-yum install httpd git -y
-systemctl start httpd
-systemctl status httpd
+user_data = base64encode(<<-EOF
+#!/bin/bash
+apt update -y
+apt install apache2 git -y
+systemctl start apache2
+systemctl enable apache2
 cd /var/www/html
+rm -f index.html
 git clone https://github.com/karishma1521success/swiggy-clone.git
-mv swiggy-clone/* .
+cp -r swiggy-clone/* .
+chown -R www-data:www-data /var/www/html
+chmod -R 755 /var/www/html
+systemctl restart apache2
+EOF
+)
