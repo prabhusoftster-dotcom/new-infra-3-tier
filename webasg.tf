eeb79632c64ac46e47ec122d8ae3f7fb44085506
prabhusoftster-dotcom <prabhusoftster@gmail.com>
2026-06-02 18:00:05 +0530

    Update webasg.tf

--- webasg.tf
+++ webasg.tf
@@ -15,7 +15,7 @@ resource "aws_autoscaling_group" "swiggy-web-asg" {
 resource "aws_launch_template" "swiggy-web-template" {
   name_prefix   = "swiggy-web-template"
   image_id      = "ami-07a00cf47dbbc844c"
-  instance_type = "t3.small"
+  instance_type = "t2.small"
 
   network_interfaces {
     associate_public_ip_address = true
