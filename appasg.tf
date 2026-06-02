b2e5e291cf2076317accd6555c987a9547d4a84f
prabhusoftster-dotcom <prabhusoftster@gmail.com>
2026-06-02 17:59:51 +0530

    Update appasg.tf

--- appasg.tf
+++ appasg.tf
@@ -21,7 +21,7 @@ resource "aws_autoscaling_group" "swiggy-app-asg" {
 resource "aws_launch_template" "swiggy-app-template" {
   name_prefix   = "swiggy-app-template"
   image_id      = "ami-07a00cf47dbbc844c"
-  instance_type = "t3.small"
+  instance_type = "t2.small"
 
   network_interfaces {
     security_groups             = [aws_security_group.swiggy-ec2-asg-sg-app.id]
